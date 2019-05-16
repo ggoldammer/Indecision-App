61c90c1a79e631b229e65f542968be1953d85eaf
Gabriel Viveros-Goldammer <gabrielgoldammer@gmail.com>
2019-05-15 23:29:45 -0700

    Adjustments to JSX layout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,20 @@
 console.log("App.js is running!");
 
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    )
 );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
